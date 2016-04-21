08527545dfbf027137726ba71bff88d391f54b29
qpelit <qpelit@greeny.cs.tlu.ee>
2016-04-21 11:08:43 +0300

    added last lesson files

---
+++ login.php
@@ -0,0 +1,79 @@
+<?php
+	require_once("functions.php");
+	
+	//RESTRICTION - NOT LOGGED IN
+	if(isset($_SESSION["user_id"])){
+		//redirect user to restricted page
+		header("Location: restrict.php");
+	}
+	
+	//login button clicked
+	if(isset($_POST["login"])){
+		
+		//login
+		
+		echo "logging in ...";
+		
+		//the username and password are not empty
+		if(!empty($_POST["username"]) && !empty($_POST["password"])){
+			
+			//save to db
+			
+			login($_POST["username"], $_POST["password"]);
+			
+		}else{
+			
+			echo "both fields are required!";
+			
+		}
+		
+	
+	//signup button clicked
+	}else if(isset($_POST["signup"])){
+		
+		//signup
+		
+		echo "signing up ...";
+		
+		//the username and password are not empty
+		if(!empty($_POST["username"]) && !empty($_POST["password"]) && !empty($_POST["name"])){
+			
+			//save to db
+			
+			signup($_POST["username"], $_POST["password"], $_POST["name"]);
+			
+		}else{
+			
+			echo "both fields are rquired!";
+			
+		}
+		
+		
+	}
+
+
+?>
+
+
+
+<h1>Log in</h1>
+<form method="POST">
+	
+	<input type="text" placeholder="username" name="username">
+	<input type="password" placeholder="password" name="password">
+	
+	<input type="submit" name="login" value="Log in">
+	
+</form> 
+
+
+<h1>Sign up</h1>
+<form method="POST">
+	
+	<input type="text" placeholder="First and Last name" name="name">
+	<input type="text" placeholder="username" name="username">
+	<input type="password" placeholder="password" name="password">
+	
+	<input type="submit" name="signup" value="Sign up">
+	
+</form> 
