3c0b41fa09baa04a64c16a8c4db7f6033e81a466
Vadim Kozlov <shikter@users.noreply.github.com>
2016-05-05 11:50:49 +0300

    Menu now in header + Style CSS menu added + Homepage for login function 2

--- app_reservation.php
+++ app_reservation.php
@@ -36,42 +36,6 @@
 ?>
 
 <?php require_once("header.php");?>
-
-<<<<<<< HEAD
-=======
-
-
-<figure id="tlu_logo"><img border=none src="http://www.tlu.ee/~shikter/ristmed2/images/TLU_logo.jpg" alt="TLU" width="200"></figure>
-<br>
-
-<nav class="navbar navbar-default">
-  <div class="container-fluid">
-    <!-- Brand and toggle get grouped for better mobile display -->
-    <div class="navbar-header">
-      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-        <span class="sr-only">Toggle navigation</span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-      </button>
-      <a class="navbar-brand" href="#">Little Estonia</a>
-    </div>
-
-    <!-- Collect the nav links, forms, and other content for toggling -->
-    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-	
-      <ul class="nav nav-tabs">
-	  
-        <li><a href="app_message.php">Message APP</a></li>
-		<li class="active"><a href="app_reservation.php">Order APP</a></li>
-		<li><a href="tables.php">Tables</a></li>
-		
-      </ul>
-
-    </div><!-- /.navbar-collapse -->
-  </div><!-- /.container-fluid -->
-</nav>
->>>>>>> a0669cccf35f5548b19230faa5e38f5dca62fc1c
 	
 	<?php
 	$current_time_with_fix = time() + (10 * 60 + 58);
@@ -104,15 +68,7 @@
 		}
 	
 	?>
-<<<<<<< HEAD
-		
-=======
 
-
-
-
-
->>>>>>> a0669cccf35f5548b19230faa5e38f5dca62fc1c
 	<h2>Reservation form:</h2>
 		<div id="errors" style="color: red;"></div>
 			<div class="row">
