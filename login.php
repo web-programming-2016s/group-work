c7fd3cef0326a142082830a1aaf674491f3dd017
Vadim Kozlov <shikter@users.noreply.github.com>
2016-05-08 19:55:04 +0300

    Showing example of input for Login

--- login.php
+++ login.php
@@ -32,18 +32,25 @@
        <h1>Log in</h1>
 <form method="POST">
 
-	<input type="text" id="text3" placeholder="Username" name="username">
+	<!-- <input type="text" id="text3" placeholder="Username" name="username"> -->
+	
+	<span class="input input--kuro">
+		<input class="input__field input__field--kuro" type="text" id="input-7" />
+				<label class="input__label input__label--kuro" for="input-7">
+					<span class="input__label-content input__label-content--kuro">Username</span>
+				</label>
+	
+	<br><br>
 	<br><br>
 	<input type="password" id="text3" placeholder="Password" name="password">
 	<br><br>
 	<input type="submit" id="login" name="login" value="Log in">
-	
+	<br><br>
 
 
 </form>
      
        
-        <p>
          <h1>Create User Name</h1>
 <form method="POST">
 <!-- <form>-->
@@ -55,20 +62,22 @@
 	<input type="Last_Name" id=text3 placeholder="Last Name" name="Last_Name">
 	<br><br>
 	<input type="submit" id= submit name="signup" value="Sign up">
-	
+	<br>
 	
 
 </form>
-       </p> 
+
        <br/>
 </div>
 
-<span class="input input--kuro">
+<!--
+				<span class="input input--kuro">
 					<input class="input__field input__field--kuro" type="text" id="input-7" />
+					
 					<label class="input__label input__label--kuro" for="input-7">
 						<span class="input__label-content input__label-content--kuro">Username</span>
 					</label>
-
+-->
 
 
 
