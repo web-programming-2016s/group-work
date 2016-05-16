c41f1d0db860b52acc166688cfaf3d378553d57a
Karli Jaanson <karlijaanson@users.noreply.github.com>
2016-05-16 08:40:38 +0300

    Update about.php

--- about.php
+++ about.php
@@ -43,17 +43,28 @@
 
 
 
-
-<small>Made by Karli Jaanson & Elis Pihl</small>
-
-
+</ul>The purpose of the project is to create an app where all the photographers and photo enthusiasts can easily rent out their equipment.  Why we think it is needful? It is create solution for professional photographers and at the same time for  those who just have passion for photography as a hobby. Photo equipment is generally expensive and if photography isn`t your everyday job then there is no point to buy yourself every part of equipment and high-priced lens. And even professional photographers time to time need some extra equipment that they commonly don`t use daily. Instead of buying, you can rent everything you need and save money.  Or if you are having  equipment that you don`t need everyday then just load it up to our app and earn some extra money!
+<p>
+<p>
+Similar applications/web services:
+RentMarket – possibility to rent tools for building. You can add pictures of your item and upload these for renting and you can fix the price.<p>
+<p>
+<p>In generally our app is like every other renting app but our difference lies in our target group -  it is for photographers. There is no other app for photoaquipment renting from person to person.<p>
+<p>
+<p>Functionalities of the app:
+	user creating
+	uploading photos of your items
+	adding items
+	adding photos</p>
+	
 </center>
 
 
 
 
-
+<p>Karli Jaanson, Elis Pihl</p>
 <h5>Used source: http://alt-web.com/TUTORIALS/</h5>
+
 
 
 </body>
